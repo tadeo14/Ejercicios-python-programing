4edfaef28686e84811f7b5c0af2666f399a3c3c8
tadeo <tadeoacosta14@gmail.com>
2024-09-15 01:03:52 -0300

    ejercicio suma

---
+++ ejercicio3.py
@@ -0,0 +1,18 @@
+"""
+El range me permite crear la "idea de un conjunto"
+
+no es una lista
+no es un diccionario
+no es una tupla
+
+....Pero puede ser iterado 
+
+range(final)
+range(inicial,final)    
+range(inicio,final, inc/dec)
+
+
+"""
+
+for n in range(5,20,5):
+    print(n)
