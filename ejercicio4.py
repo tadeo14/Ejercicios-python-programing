5cbc999eaf7b2960d7588f34fbc1566fa2e308e8
tadeo <tadeoacosta14@gmail.com>
2024-09-15 01:48:17 -0300

    funciones de suma

--- ejercicio4.py
+++ ejercicio4.py
@@ -5,7 +5,26 @@ def sumar (a,b):
     c= a + b
     return c
 
+def suma2 (a=5, b=4):
+    """
+    La funcion sumar sirve para 
+    sumar 2 parametros (a y b) 
+    luego el resultado se muestra en pantalla 
+
+    ejemplo: 
+    >>> suma2(5,4)
+    >>> 9 
+    """
+    c = a + b
+    print(c)
+
 
 ##########################
 
-sumar(10,2)
+w = sumar(10,2)
+q = sumar(50,30)
+
+suma2(b=10)
+
+print(w)
+print(q)
