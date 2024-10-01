acd8ab0766de4a06a8651f34b2d0ea51be4113a8
tadeo <tadeoacosta14@gmail.com>
2024-10-01 00:18:13 -0300

    creamos un proyecto

---
+++ ejemplo.py
@@ -0,0 +1,7 @@
+
+import sys
+
+#lista 
+argumentos = sys.argv
+
+print("Hola:" ,argumentos[1])
--- regresiva.py
+++ regresiva.py
@@ -1,6 +1,21 @@
+import sys
+import time
 
-import time 
 
-for n in range (10,0,-1):
-    print (n) 
-    time.sleep(1)  # wait for 1 second
+#cuenta regresiva debera llegar a 0
+
+argumentos = sys.argv
+
+try:
+    desde = int(argumentos[1])
+except IndexError:
+    print("no hay argumentos")
+    sys.exit()
+except ValueError:
+    print("el argumento no es un entero")
+    sys.exit()
+
+
+for n in range(desde,0,-1):
+    print(n)
+    time.sleep(1)
